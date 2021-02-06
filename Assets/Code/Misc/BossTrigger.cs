0e4224f0e4e61294a5d3b5a59ee69f19f94d778e
clear <nishchai@outlook.com>
2021-02-06 14:44:57 -0500

    Fixed Ordering Issues. Boss Changes

---
+++ Assets/Code/Misc/BossTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTrigger : MonoBehaviour
+{
+
+    private BoxCollider2D bCollider;
+    [SerializeField] private GameObject boss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) 
+        {
+            boss.SetActive(true);
+        }
+    }
+
+}
