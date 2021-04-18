d216b5d6746b8d5e2ef5dd79284d61fb7d584ab6
clear <nishchai@outlook.com>
2021-04-17 23:54:41 -0400

    Edited Sprites | Visuals. Edited Schedule + Level

---
+++ Assets/Code/Enemies/FlyingHead/FlyingHead.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingHead : MonoBehaviour
+{
+
+    [SerializeField] private Transform Player;
+    [SerializeField] private float dashSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<EnemyBase>().shouldTrack) 
+        {
+            // add dashing
+        }
+    }
+}
